3e1acec5a76d68e6ad0e50721c4b7da540ac7112
NShoSaga <31090682+NShoSaga@users.noreply.github.com>
2019-07-28 19:10:10 +0900

    first commit

--- app/src/main/java/either/com/mabeeemscanner/MainActivity.java
+++ app/src/main/java/either/com/mabeeemscanner/MainActivity.java
@@ -1,99 +1,29 @@
 package either.com.mabeeemscanner;
 
-import android.Manifest;
-import android.app.Activity;
-import android.bluetooth.BluetoothAdapter;
-import android.bluetooth.BluetoothDevice;
-import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.os.Build;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.widget.Toast;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-
-    public static final String TAG = "MainActivity";
-    private BluetoothAdapter mBtAdapter = null;
-
-    private static final int REQUEST_SELECT_DEVICE = 1;
-    private static final int REQUEST_ENABLE_BT = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            if (checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                // 権限がない場合はリクエスト
-                requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, 1);
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
             }
-        }
-
-        mBtAdapter = BluetoothAdapter.getDefaultAdapter();
-        if (null == mBtAdapter) {
-            return;
-        }
-
-        BtConnect();
+        });
     }
 
-    @Override
-    public void onResume() {
-        Log.d(TAG, "onResume");
-
-        super.onResume();
-    }
-
-    private void BtConnect() {
-        if (!mBtAdapter.isEnabled()) {
-            Log.i(TAG, "onClick - BT not enabled yet");
-            Intent enableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-            startActivityForResult(enableIntent, REQUEST_ENABLE_BT);
-        } else {
-            Intent newIntent = new Intent(MainActivity.this, DeviceListActivity.class);
-            startActivityForResult(newIntent, REQUEST_SELECT_DEVICE);
-        }
-    }
-
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        switch (requestCode) {
-
-            case REQUEST_SELECT_DEVICE:
-                //When the DeviceListActivity return, with the selected device address
-                /*
-                if (resultCode == Activity.RESULT_OK && data != null) {
-                    String deviceAddress = data.getStringExtra(BluetoothDevice.EXTRA_DEVICE);
-                    m_deviceAddress = new String(deviceAddress);
-                    mDevice = BluetoothAdapter.getDefaultAdapter().getRemoteDevice(deviceAddress);
-
-//                    connect(this, mDevice);
-                    mBleService.connect(m_deviceAddress);
-                    mDeviceAddButton.setText(mDevice.getName());
-//                    Log.d(TAG, "... onActivityResultdevice.address==" + mDevice + "mserviceValue" + mService);
-//                    ((TextView) findViewById(R.id.deviceName)).setText(mDevice.getName()+ " - connecting");
-//                    mService.connect(m_deviceAddress);
-                }
-                */
-                break;
-            case REQUEST_ENABLE_BT:
-                // When the request to enable Bluetooth returns
-                if (resultCode == Activity.RESULT_OK) {
-                    Toast.makeText(this, "Bluetooth has turned on ", Toast.LENGTH_SHORT).show();
-
-                } else {
-                    // User did not enable Bluetooth or an error occurred
-                    Log.d(TAG, "BT not enabled");
-                    Toast.makeText(this, "Problem in BT Turning ON ", Toast.LENGTH_SHORT).show();
-                    finish();
-                }
-                break;
-            default:
-                Log.e(TAG, "wrong request code");
-                break;
-        }
-    }
 }
